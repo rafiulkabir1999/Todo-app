e8e8447074acfde650bb2e57b5f70938d4cdce74
Nahid Ul Kabir <rafi.eee.1999@gmail.com>
2023-07-21 22:44:45 +0600

    design added

---
+++ script.js
@@ -0,0 +1,18 @@
+const todo = [
+    {
+        name:"Reading article",
+        status:false,
+    },
+    {
+        name:"Playing football",
+        status:false,
+    }
+]
+
+document.getElementById("add").addEventListener('click',() => {
+    document.getElementById('INPUT').classList.toggle('Show')
+})
+
+document.getElementById('button').addEventListener('click' , () => {
+    
+})
